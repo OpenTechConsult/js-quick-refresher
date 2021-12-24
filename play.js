7ddcbebf7c94b55a6a40b71f09e4c6af5e6341ac
OpenTech-Consult <opentech.consult@gmail.com>
2021-12-24 13:55:09 +0000

    Created variables and functions and displayed them in the console. Closes #1

---
+++ play.js
@@ -0,0 +1,21 @@
+// declare a variable
+var name = 'Sandro'
+
+// creat a number variable
+var age = 39
+
+// create a boolean variable
+var hasHobbies = true
+
+// create or declare a function
+function summarizeUser(userName, userAge, userHasHobbies) {
+    return 'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobbies
+}
+
+
+
+// display the variable on the console
+console.log(name)
+console.log(age)
+console.log(hasHobbies)
+console.log(summarizeUser(name, age, hasHobbies))
