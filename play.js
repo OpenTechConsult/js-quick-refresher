2e7bd96d8f6cde7ce47081a4d291e419bc83cda9
OpenTech-Consult <opentech.consult@gmail.com>
2021-12-26 21:14:23 +0000

    Arrays Objects and Reference Types
    demonstrated how arrays and objects are reference types
    
    closes #7

--- play.js
+++ play.js
@@ -9,9 +9,12 @@ const person = {
 
 
 const hobbies = ['Sports', 'Cooking'];
-for (const hobby of hobbies) {
-  console.log(hobby);
-}
+// for (const hobby of hobbies) {
+//   console.log(hobby);
+// }
 
-console.log(hobbies.map( hobby => 'Hobby: ' + hobby));
+// console.log(hobbies.map( hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+hobbies.push('Programming');
 console.log(hobbies);
