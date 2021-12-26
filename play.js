0718da9c6b76fc12ad9392f0eb8f62ba96be9a4e
OpenTech-Consult <opentech.consult@gmail.com>
2021-12-26 23:03:22 +0000

    Destructuring
    saw how objects, variables, and array destructuring works
    
    closes #9

--- play.js
+++ play.js
@@ -7,22 +7,18 @@ const person = {
   }
 };
 
-copiedPerson = {...person};
-console.log(copiedPerson);
-
-
-const hobbies = ['Sports', 'Cooking'];
-// for (const hobby of hobbies) {
-//   console.log(hobby);
-// }
-
-// console.log(hobbies.map( hobby => 'Hobby: ' + hobby));
-// console.log(hobbies);
-
-const copiedArray = [...hobbies]
-console.log(copiedArray);
-
-const toArray = (...args) => {
-  return args
+// object destructuring
+const printName = ({name}) => {
+  console.log(name);
 }
-console.log(toArray(1, 2, 3, 4));
+
+printName(person);
+
+// variable destructuring
+const {name, age} = person;
+console.log(name, age);
+
+// array destructuring
+const hobbies = ['Sports', 'Cooking'];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
